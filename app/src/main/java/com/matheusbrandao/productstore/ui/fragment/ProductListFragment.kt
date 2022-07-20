86a0bc8d9d1affd7be82969d945a5004ca3d4a38
matheus.brandao <matheusb1990@gmail.com>
2022-07-20 14:44:06 -0300

    adding navigation and viewBinding dependencies
    also creating fragment to show list of products

---
+++ app/src/main/java/com/matheusbrandao/productstore/ui/fragment/ProductListFragment.kt
@@ -0,0 +1,29 @@
+package com.matheusbrandao.productstore.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.matheusbrandao.productstore.R
+import com.matheusbrandao.productstore.databinding.FragmentProductListBinding
+
+class ProductListFragment : Fragment() {
+
+    private lateinit var binding: FragmentProductListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val root = inflater.inflate(R.layout.fragment_product_list, container, false)
+        binding = FragmentProductListBinding.bind(root)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
