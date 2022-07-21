e3b5f3ba07055eb0f56f42c8869eb6d53d845d45
matheus.brandao <matheusb1990@gmail.com>
2022-07-21 09:30:41 -0300

    creating product repository

--- app/src/main/java/com/matheusbrandao/productstore/AndroidApplication.kt
+++ app/src/main/java/com/matheusbrandao/productstore/AndroidApplication.kt
@@ -3,6 +3,7 @@ package com.matheusbrandao.productstore
 import android.app.Application
 import com.matheusbrandao.productstore.di.dataSourceModule
 import com.matheusbrandao.productstore.di.presentationModule
+import com.matheusbrandao.productstore.di.repositoryModule
 import com.matheusbrandao.productstore.di.serviceModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -20,7 +21,8 @@ class AndroidApplication : Application() {
                 listOf(
                     presentationModule,
                     serviceModule,
-                    dataSourceModule
+                    dataSourceModule,
+                    repositoryModule
                 )
             )
         }
